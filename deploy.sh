7a7230b2e033a72304fb0408d6ddf22862651a2c
ericpi <ericpi@kkbox.com>
2014-12-02 20:08:17 +0800

    delete _site/deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 /bin/rm -Rf _site/*
 jekyll build
+_site/deploy.sh
+/usr/bin/git _site/* 
 /usr/bin/git commit -a
 /usr/bin/git push origin `/usr/bin/git subtree split --prefix _site`:master --force
 
