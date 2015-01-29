de6436f80e1f645e1234a16698ef1f002e5f88c7
ericpi <ericpi@kkbox.com>
2015-01-29 21:05:54 +0800

    要把 _site 都加進 commit

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 /bin/rm -Rf _site/*
 jekyll build
 /bin/rm _site/deploy.sh
-/usr/bin/git _site/* 
+/usr/bin/git add _site/* 
 /usr/bin/git commit -a
 /usr/bin/git push origin `/usr/bin/git subtree split --prefix _site`:master --force
 
